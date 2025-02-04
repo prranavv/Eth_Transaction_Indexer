6013798f10c7e36be652df11f255cd6ff6ed4180
prranavv <pranavvkumar03@gmail.com>
2025-02-04 22:49:35 +0530

    Done

--- main.go
+++ main.go
@@ -3,29 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
-	"math/big"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-//var interestedAddresses = []string{"0xeBec795c9c8bBD61FFc14A6662944748F299cAcf", "0xDFaa75323fB721e5f29D43859390f62Cc4B600b8"}
-
-var interestedAddy = "0xeBec795c9c8bBD61FFc14A6662944748F299cAcf"
+var interestedAddresses = map[string]int{"0xeBec795c9c8bBD61FFc14A6662944748F299cAcf": 1, "0xDFaa75323fB721e5f29D43859390f62Cc4B600b8": 1}
 
 func indexer() {
 	cl, err := ethclient.Dial("https://eth-client")
 	if err != nil {
 		panic(err)
 	}
-	blockNumber := big.NewInt(int64(21774224))
-	block, err := cl.BlockByNumber(context.Background(), blockNumber)
-	if err != nil {
-		panic(err)
-	}
+	for {
+		//blockNumber := big.NewInt(int64(21774224))
+		block, err := cl.BlockByNumber(context.Background(), nil)
+		if err != nil {
+			panic(err)
+		}
 
-	for _, transaction := range block.Transactions() {
-		if transaction.To().Hex() == string(interestedAddy) {
-			fmt.Println(transaction.Hash())
+		for _, transaction := range block.Transactions() {
+			_, ok := interestedAddresses[transaction.To().Hex()]
+			if ok {
+				fmt.Println(transaction.Value())
+			}
 		}
 	}
 
